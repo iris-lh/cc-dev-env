caf07674f048bb3db3d40e70d53d1743679c50e5
Isaac Helbling <imhelbling@gmail.com>
2017-07-02 18:21:19 -0400

    included original moonc executable in project

--- bin/moonc.js
+++ bin/moonc.js
@@ -1,7 +1,7 @@
 const execa = require('execa');
 
 function moonc(moon) {
-  return execa.shellSync(`echo ${moon} | moonc --`).stdout
+  return execa.shellSync(`echo ${moon} | ${__dirname}/moonc --`).stdout
 }
 
 module.exports = moonc
