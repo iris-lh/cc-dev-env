7a2cb0bfb7f2482a56cc523d902cb271a1fd8368
Isaac Helbling <imhelbling@gmail.com>
2017-07-02 18:24:31 -0400

    added comment

--- bin/moonc.js
+++ bin/moonc.js
@@ -1,6 +1,7 @@
 const execa = require('execa');
 
 function moonc(moon) {
+  // original moonc should live in the directory with this wrapper
   return execa.shellSync(`echo ${moon} | ${__dirname}/moonc --`).stdout
 }
 
